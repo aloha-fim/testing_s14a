7d4ac6ee24f8e98a89a23b3aed7be9c598b537fd
aloha-fim <frederec.im@gmail.com>
2023-07-19 16:20:51 -0400

    create models with loginuser

--- project_solar/__init__.py
+++ project_solar/__init__.py
@@ -1,7 +1,34 @@
 # project_solar/__init__.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+from os import environ
+
+load_dotenv(".env")
+
 
 app = Flask(__name__)
+
+#########################
+###### DATABASE SETUP ###
+#########################
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = environ.get("SECRET_KEY")
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+#################
+# LOGIN CONFIGS
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
 
 # register blueprints and import
 from project_solar.core.views import core
