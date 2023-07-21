345a86c7db8cb18a577e1fd31dac47354d70ec5e
aloha-fim <frederec.im@gmail.com>
2023-07-20 22:41:17 -0400

    finish crud with bug fixes

--- project_solar/core/views.py
+++ project_solar/core/views.py
@@ -1,10 +1,17 @@
 from flask import render_template,request,Blueprint
+from project_solar.models import SolarPost
 
 core = Blueprint('core',__name__)
 
 @core.route('/')
 def index():
-    return render_template('index.html')
+    '''
+    This is the home page view calling paginate.
+    '''
+    page = request.args.get('page', 1, type=int)
+    solar_posts = SolarPost.query.order_by(SolarPost.date.desc()).paginate(page=page, per_page=10)
+    return render_template('index.html',solar_posts=solar_posts)
+
 
 @core.route('/info')
 def info():
--- project_solar/solar_posts/views.py
+++ project_solar/solar_posts/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, request, redirect, Blueprint
+from flask import render_template, url_for, flash, abort, request, redirect, Blueprint
 from flask_login import current_user, login_required
 from project_solar import db
 from project_solar.models import SolarPost
@@ -25,7 +25,49 @@ def create_post():
     return render_template('create_post.html',form=form)
 
 #VIEW
+@solar_posts.route('/<int:solar_post_id>')
+def solar_post(solar_post_id):
+    solar_post = SolarPost.query.get_or_404(solar_post_id)
+    return render_template('solar_post.html', title=solar_post.title,
+                           date=solar_post.date,post=solar_post)
+
 
 #UPDATE
+@solar_posts.route('/<int:solar_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(solar_post_id):
+    solar_post = SolarPost.query.get_or_404(solar_post_id)
+
+    if solar_post.author != current_user:
+        abort(403)
+
+    form = SolarPostForm()
+
+    if form.validate_on_submit():
+
+        solar_post.title=form.title.data
+        solar_post.text=form.text.data
+        db.session.commit()
+        flash('Solar Post Updated')
+        return redirect(url_for('solar_posts.solar_post',solar_post_id=solar_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = solar_post.title 
+        form.text.data = solar_post.text
+    
+    return render_template('create_post.html',title='Update',form=form)
+
 
 #DELETE
+@solar_posts.route('/<int:solar_post_id>/delete',methods=['POST'])
+@login_required
+def delete_post(solar_post_id):
+
+    solar_post = SolarPost.query.get_or_404(solar_post_id)
+    if solar_post.author != current_user:
+        abort(403)
+
+    db.session.delete(solar_post)
+    db.session.commit()
+    flash('Solar Post Deleted')
+    return redirect(url_for('core.index'))
