bc3c7216212e00b516cf42433d2352b4808ded5b
aloha-fim <frederec.im@gmail.com>
2023-07-21 18:00:33 -0400

    add linear regression

--- project_solar/solar_posts/views.py
+++ project_solar/solar_posts/views.py
@@ -4,7 +4,23 @@ from project_solar import db
 from project_solar.models import SolarPost
 from project_solar.solar_posts.forms import SolarPostForm
 
+import joblib
+# from sklearn import linear_model
+# from sklearn.metrics import r2_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import pandas as pd
+import sklearn
+
+
 solar_posts = Blueprint('solar_posts',__name__)
+
+# modellg = joblib.load("regr.pkl")
+
+file = open('C:/Users/frede/harvard/s14a/regr.pkl', 'rb')
+modellg = joblib.load(file)
+file.close()
 
 #CREATE
 @solar_posts.route('/create', methods=['GET','POST'])
@@ -71,3 +87,30 @@ def delete_post(solar_post_id):
     db.session.commit()
     flash('Solar Post Deleted')
     return redirect(url_for('core.index'))
+
+@solar_posts.route("/inputprediction")
+def input_predict():
+    return render_template('results.html')
+
+
+@solar_posts.route('/predict', methods = ['POST', 'GET'])
+
+def make_prediction():
+    
+    if request.method=='POST':
+
+        entered_li = []
+
+        beds = request.form.get('beds')
+        baths = request.form.get('baths')
+        sqft = request.form.get('sqft')
+
+
+        # make prediction
+        entered_li = ([beds, baths, sqft])
+
+        prediction = modellg.predict(np.array(entered_li).astype(float).reshape(1, -1))
+        
+        label = str(np.squeeze(prediction.round(2)))
+
+        return render_template('results.html', label=label)
