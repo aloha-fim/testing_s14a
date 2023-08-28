063080b75a4bd659690176151027398cabc0e0e8
aloha-fim <frederec.im@gmail.com>
2023-08-28 12:06:19 -1000

    listing skeleton

---
+++ project_solar/listing/views.py
@@ -0,0 +1,5 @@
+from flask import render_template, url_for, flash, abort, request, redirect, Blueprint
+from flask_login import current_user, login_required
+from project_solar import db
+from project_solar.models import ListingPost
+from project_solar.listing.forms import ListingPostForm
--- project_solar/models.py
+++ project_solar/models.py
@@ -51,3 +51,22 @@ class SolarPost(db.Model):
         return f"Post ID: {self.id} -- Date: {self.date} -- {self.title}"
     
 
+class ListingPost(db.Model):
+    
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+
+    date = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    title = db.Column(db.String(140),nullable=False)
+    text = db.Column(db.Text,nullable=False)
+
+    def __init__(self,title,text,user_id):
+        self.title = title
+        self.text = text
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Post ID: {self.id} -- Date: {self.date} -- {self.title}"
+    
