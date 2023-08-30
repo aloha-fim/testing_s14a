6b0d191aae3b85f796cb111501259324b27cac7d
aloha-fim <frederec.im@gmail.com>
2023-08-29 19:09:11 -0700

    refactor with header file to index-directory

--- project_solar/listings/views.py
+++ project_solar/listings/views.py
@@ -2,4 +2,7 @@ from flask import render_template, url_for, flash, abort, request, redirect, Blu
 from flask_login import current_user, login_required
 from project_solar import db
 from project_solar.models import ListingPost
-from project_solar.listing.forms import ListingPostForm
+from project_solar.listings.forms import ListingPostForm
+
+
+listings = Blueprint('listings',__name__)
