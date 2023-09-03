559d33cc8a3c57616fc3a08061e0257e93547737
aloha-fim <frederec.im@gmail.com>
2023-09-02 21:37:31 -0700

    add model to listing

--- project_solar/models.py
+++ project_solar/models.py
@@ -59,14 +59,35 @@ class ListingPost(db.Model):
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
 
     date = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-    title = db.Column(db.String(140),nullable=False)
-    text = db.Column(db.Text,nullable=False)
+    listing_type = db.Column(db.String(140),nullable=False)
+    listing_name = db.Column(db.String(140),nullable=False)
+    amount_land = db.Column(db.String(140),nullable=False)
+    short_description = db.Column(db.String(140),nullable=False)
+    country = db.Column(db.String(140),nullable=False)
+    state = db.Column(db.String(140),nullable=False)
+    city = db.Column(db.String(140),nullable=False)
+    postal_code = db.Column(db.String(140),nullable=False)
+    street = db.Column(db.String(140),nullable=False)
+    longitude = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Integer, primary_key=True)
+    thumbnail_image = db.Column(db.String(140),nullable=False)
+    gallery_image = db.Column(db.String(140),nullable=False)
+    amenities = db.Column(db.String(140),nullable=False)
+    listing_description = db.Column(db.String(140),nullable=False)
+    total_floor = db.Column(db.String(140),nullable=False)
+    total_room = db.Column(db.String(140),nullable=False)
+    room_area = db.Column(db.String(140),nullable=False)
+    room_name = db.Column(db.String(140),nullable=False)
+    room_thumbnail_image = db.Column(db.String(140),nullable=False)
+    room_price = db.Column(db.String(140),nullable=False)
+    discount = db.Column(db.Integer, primary_key=True)
+    additional_info = db.Column(db.String(140),nullable=False)
 
-    def __init__(self,title,text,user_id):
-        self.title = title
-        self.text = text
+    def __init__(self,listing_type,listing_name,user_id):
+        self.listing_type = listing_type
+        self.listing_name = listing_name
         self.user_id = user_id
 
     def __repr__(self):
-        return f"Post ID: {self.id} -- Date: {self.date} -- {self.title}"
+        return f"Post ID: {self.id} -- Date: {self.date} -- {self.listing_name}"
     
