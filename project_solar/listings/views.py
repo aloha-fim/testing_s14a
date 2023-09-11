7a0dd5870e3b324212af63020e1c8779a01c886a
aloha-fim <frederec.im@gmail.com>
2023-09-11 11:25:20 -1000

    model, views, forms for pictures

--- project_solar/listings/views.py
+++ project_solar/listings/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, url_for, flash, abort, request, redirect, Blueprint, current_app
 from flask_login import current_user, login_required
 from project_solar import db
-from project_solar.models import ListingPost
-from project_solar.listings.forms import ListingPostForm
+from project_solar.models import ListingPost, ListingPictures
+from project_solar.listings.forms import ListingPostForm, ListingPictureForm
 import os
 
 
@@ -19,10 +19,6 @@ def add_listing():
 
     form = ListingPostForm()
 
-    if request.method == 'POST':
-        for f in request.files.getlist('file'):
-            f.save(os.path.join(os.path.join(listings.root_path, 'upload'), f.filename))
-
     if form.validate_on_submit():
         listing = ListingPost(user_id=current_user.id,
                     listing_type=form.listing_type.data,
@@ -36,15 +32,12 @@ def add_listing():
                     street = form.street.data,
                     longitude = form.longitude.data,
                     latitude = form.latitude.data,
-                    thumbnail_image = form.thumbnail_image.data,
-                    gallery_image = form.gallery_image.data,
                     amenities = form.amenities.data,
                     listing_description = form.listing_description.data,
                     total_floor = form.total_floor.data,
                     total_room = form.total_room.data,
                     room_area = form.room_area.data,
                     room_name = form.room_name.data,
-                    room_thumbnail_image = form.room_thumbnail_image.data,
                     room_price = form.room_price.data,   
                     discount = form.discount.data,                                                                                                                   
                     additional_info=form.additional_info.data)
@@ -52,6 +45,30 @@ def add_listing():
         db.session.add(listing)
         db.session.commit()
         flash('Thanks for Listing!')
-        return redirect(url_for('core.start'))
+        return redirect(url_for('listing.pictures'))
     
     return render_template('add-listing.html',form=form)
+
+
+@listings.route('/add_picture_listing',methods=['GET','POST'])
+@login_required
+def add_picture_listing():
+
+    form = ListingPictureForm()
+
+    if request.method == 'POST':
+        for f in request.files.getlist('file'):
+            f.save(os.path.join(os.path.join(listings.root_path, 'upload'), f.filename))
+
+    if form.validate_on_submit():
+        listing_pictures = ListingPictures(user_id=current_user.id,
+                    thumbnail_image = form.thumbnail_image.data,
+                    gallery_image = form.gallery_image.data,
+                    room_thumbnail_image = form.room_thumbnail_image.data)
+        
+        db.session.add(listing_pictures)
+        db.session.commit()
+        flash('Thanks for Listing!')
+        return redirect(url_for('core.start'))
+    
+    return render_template('add-pictures-listing.html',form=form)
