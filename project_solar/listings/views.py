f1187aad53fc6d7432d90a961eec8150c9df8112
aloha-fim <frederec.im@gmail.com>
2023-10-19 15:13:09 +0800

    stripe integration

--- project_solar/listings/views.py
+++ project_solar/listings/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, abort, request, redirect, Blueprint, current_app
+from flask import render_template, url_for, flash, abort, request, redirect, Blueprint, current_app, jsonify
 from flask_login import current_user, login_required
 from werkzeug.utils import secure_filename
 from project_solar import db
@@ -7,6 +7,7 @@ from project_solar.listings.forms import ListingPostForm, ListingSecondPostForm,
 from project_solar.listings.picture_handler import add_listing_pic
 from project_solar.listings.picture_format import allowed_file
 import os
+import stripe
 
 UPLOAD_FOLDER = 'static\listing_pics'
 
@@ -15,13 +16,57 @@ UPLOAD_FOLDER = 'static\listing_pics'
 listings = Blueprint('listings',__name__, template_folder="templates")
 current_app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# stripe live
 stripe_keys = {
-	'secret_key': 'pk_live_JtJxCVIshzT8LGrJOJFTAnPv007SAuwvja',
-	'publishable_key': 'sk_live_E3jNwV9dCmr9vUkzD69USGgV00FINYaBRs'
+    "secret_key": os.environ["STRIPE_SECRET_KEY"],
+    "publishable_key": os.environ["STRIPE_PUBLISHABLE_KEY"],
 }
 
-#stripe.api_key = stripe_keys['secret_key']
+stripe.api_key = stripe_keys["secret_key"]
+
+
+@listings.route("/config")
+def get_publishable_key():
+    stripe_config = {"publicKey": stripe_keys["publishable_key"]}
+    return jsonify(stripe_config)
+
+
+@listings.route("/create-checkout-session")
+def create_checkout_session():
+    domain_url = "http://127.0.0.1:5000/"
+    stripe.api_key = stripe_keys["secret_key"]
+
+    try:
+        # Create new Checkout Session for the order
+        # Other optional params include:
+        # [billing_address_collection] - to display billing address details on the page
+        # [customer] - if you have an existing Stripe Customer ID
+        # [payment_intent_data] - capture the payment later
+        # [customer_email] - prefill the email input in the form
+        # For full details see https://stripe.com/docs/api/checkout/sessions/create
+
+        # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+        checkout_session = stripe.checkout.Session.create(
+            success_url=domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=domain_url + "cancelled",
+            payment_method_types=["card"],
+            mode="payment",
+            line_items=[
+                {
+                    "quantity": 1,
+                    "price_data": {
+                        "currency": "usd",
+                        "unit_amount": 2000,
+                        "product_data": {
+                            "name": "T-shirt"
+                        },
+                    },
+                }
+            ]
+        )
+        return jsonify({"sessionId": checkout_session["id"]})
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
 
 @listings.route('/booking_confirm')
 def booking_confirm():
