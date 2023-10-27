18683b8c13dd3e141d65642671943274d06b9531
aloha-fim <frederec.im@gmail.com>
2023-10-27 19:42:07 +0800

    account booking view update

--- project_solar/accounts/views.py
+++ project_solar/accounts/views.py
@@ -1,15 +1,36 @@
 from flask import render_template, url_for, flash, abort, request, redirect, Blueprint, current_app
-from flask_login import current_user, login_required
+from flask_login import current_user, login_required, login_user, logout_user
 from project_solar import db
-from project_solar.models import ListingPost, ListingSecondPost, ListingPictures
+from project_solar.models import ListingPost, ListingSecondPost, ListingPictures, User
 from project_solar.listings.forms import ListingPostForm, ListingSecondPostForm, ListingPictureForm
+from project_solar.users.forms import RegistrationForm, LoginForm, UpdateUserForm 
+from project_solar.users.picture_handler import add_profile_pic
 import os
 
 accounts = Blueprint('accounts',__name__, template_folder="templates")
 
-@accounts.route('/account_bookings')
+@accounts.route('/account_bookings',methods=['GET','POST'])
+@login_required
 def account_bookings():
     # page = request.args.get('page',1,type=int)
-    # posts = db.session.query(ListingPost,ListingSecondPost,ListingPictures).filter(ListingPost.id==ListingSecondPost.id,ListingPost.id==ListingPictures.id).order_by(ListingPost.date.desc()).paginate(page=page,per_page=9)   
+    # posts = db.session.query(ListingPost,ListingSecondPost,ListingPictures).filter(ListingPost.id==ListingSecondPost.id,ListingPost.id==ListingPictures.id).order_by(ListingPost.date.desc()).paginate(page=page,per_page=9)  
+    form = UpdateUserForm()
+    if form.validate_on_submit():
+    
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data,username)
+            current_user.profile_image = pic
 
-    return render_template('accounts/account-bookings.html')
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('User Account Updated!')
+        return redirect(url_for('users.account'))
+    
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for('static', filename='profile_pics/'+current_user.profile_image) 
+    return render_template('accounts/account-bookings.html',profile_image=profile_image,form=form)
