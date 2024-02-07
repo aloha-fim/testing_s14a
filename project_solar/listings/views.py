80638cb9d4ba4c011fb5afe8799f0eefa4f2d574
aloha-fim <frederec.im@gmail.com>
2024-02-07 19:24:11 +0800

    add recipe functionality to listing page

--- project_solar/listings/views.py
+++ project_solar/listings/views.py
@@ -8,6 +8,10 @@ from project_solar.listings.forms import ListingPostForm, ListingSecondPostForm,
 from project_solar.listings.picture_handler import add_listing_pic
 from project_solar.listings.picture_format import allowed_file
 import os
+#from dotenv import load_dotenv
+import google.generativeai as genai
+from PIL import Image
+
 import stripe
 
 UPLOAD_FOLDER = 'static\listing_pics'
@@ -26,6 +30,10 @@ stripe_keys = {
 }
 
 stripe.api_key = stripe_keys["secret_key"]
+
+#load_dotenv() ## load all the environment variables
+
+genai.configure(api_key=os.environ("GOOGLE_API_KEY"))
 
 
 @listings.route("/config")
@@ -188,6 +196,63 @@ def details(listing_id):
     listingPicturePosts = ListingPictures.query.filter_by(id=listing_id)
 
     return render_template('listings/tour-detail.html', listingPosts=listingPosts, listingSecondPosts=listingSecondPosts, listingPicturePosts=listingPicturePosts)
+
+
+@listings.route('/test', methods=['GET','POST'])
+## Function to load Google Gemini Pro Vision API And get response
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+def test():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({"error": "No file in the request"}), 400
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({"error":"No selected file"}), 400
+
+        filename = secure_filename(file.filename)
+        file.save(filename)
+
+        input_prompt="""
+        You are an expert in nutritionist where you need to see the food items from the image
+               and calculate the total calories, also provide the details of every food items with calories intake
+               is below format
+
+               1. Item 1 - no of calories
+               2. Item 2 - no of calories
+               ----
+               ----
+
+
+        """
+
+        question = request.form['question']
+
+        image_data=input_image_setup(filename)
+        response=get_gemini_repsonse(input_prompt,image_data,question)
+
+        return render_template('test.html', response=response)
 
 
 @listings.route('/results_list')
